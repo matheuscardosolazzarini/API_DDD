1438d7512c61eca54354dc93818341bea7477445
Matheus Cardoso Lazzarini <matheuscardosolazzarini@gmail.com>
2024-06-04 18:25:19 -0300

    Add project files.

---
+++ Dominio/Interfaces/INoticia.cs
@@ -0,0 +1,17 @@
+﻿using Dominio.Interfaces.Genericos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces
+{
+	public interface INoticia : IGenericos<INoticia>
+	{
+		Task Atualizar(Noticia noticia);
+		Task<List<Noticia>> ListarNoticias(Expression<Func<Noticia,bool>> exNoticia);
+	}
+}
---
+++ Dominio/Servicos/ServicoNoticia.cs
@@ -0,0 +1,55 @@
+﻿using Dominio.Interfaces;
+using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+	public class ServicoNoticia : IServicoNoticia
+	{
+
+		private readonly INoticia _INoticia;
+
+		public ServicoNoticia(INoticia INoticia)
+		{
+			_INoticia = INoticia;
+		}
+		public async Task AdicionaNoticia(Noticia noticia)
+		{
+			var validarTitulo = noticia.ValidarPropriedadesString(noticia.Titulo, "Titulo");
+			var validarInformacao = noticia.ValidarPropriedadesString(noticia.Informacao, "Informacao");
+
+			if (validarTitulo && validarInformacao)
+			{
+				noticia.DataAlteracao = DateTime.Now;
+				noticia.DataCadastro = DateTime.Now;
+				noticia.Ativo = true;
+				await _INoticia.Atualizar(noticia);
+			}
+		}
+
+		public async Task AtualizaNoticia(Noticia noticia)
+		{
+
+			var validarTitulo = noticia.ValidarPropriedadesString(noticia.Titulo, "Titulo");
+			var validarInformacao = noticia.ValidarPropriedadesString(noticia.Informacao, "Informacao");
+
+			if (validarTitulo && validarInformacao)
+			{
+				noticia.DataAlteracao = DateTime.Now;
+				noticia.DataCadastro = DateTime.Now;
+				noticia.Ativo = true;
+				await _INoticia.Atualizar(noticia);
+			}
+		}
+
+		public async Task<List<Noticia>> ListarNoticiasAtivas()
+		{
+			return await _INoticia.ListarNoticias(n => n.Ativo);
+		}
+	}
+}
