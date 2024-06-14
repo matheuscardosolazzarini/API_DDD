712261e56fd162e3d2e7ec803b0c56fa6f74fcbf
Matheus Cardoso Lazzarini <matheuscardosolazzarini@gmail.com>
2024-06-13 21:14:07 -0300

    adicionando o CORS

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -39,7 +39,7 @@ namespace WebAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
+			services.AddCors();
 			services.AddDbContext<Contexto>(options =>
 			 options.UseSqlServer(
 				 Configuration.GetConnectionString("DefaultConnection")));
@@ -100,6 +100,12 @@ namespace WebAPI
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+
+			var urlCliente = "http://dominiodocliente.com.br";
+			var urlCliente2 = "http://dominiodocliente.com.br";
+
+			app.UseCors(b =>b.WithOrigins(urlCliente, urlCliente2));
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
